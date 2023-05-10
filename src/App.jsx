d1af03953afa2186400fe4ca301108c68dc0d786
rcmehedi15 <hmdmehedi2016@gmail.com>
2023-05-10 12:32:22 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Outlet } from 'react-router-dom'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <App></App>
+    <Outlet></Outlet>
+  
+   
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { 
+  createBrowserRouter,RouterProvider
+} from "react-router-dom";
+import UpdateCoffee from './components/UpdateCoffee.jsx';
+import AddCoffee from './components/AddCoffee.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App></App>
+  },
+  {
+    path: 'addCoffee',
+    element: <AddCoffee></AddCoffee>
+  },
+  {
+    path: 'updateCoffe',
+    element: <UpdateCoffee></UpdateCoffee>
+  }
+])
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+     <RouterProvider router={router} />
+  </React.StrictMode>,
+)
